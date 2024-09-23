e59c83d1e659b099de82498a222d566e67b178b7
Osca Kholopha <118875945+Osca-K@users.noreply.github.com>
2024-09-23 21:32:58 +0200

    The variable defination for the functionalities

--- app/src/main/java/com/example/gaitxplore/MainActivity.kt
+++ app/src/main/java/com/example/gaitxplore/MainActivity.kt
@@ -1,31 +1,67 @@
 package com.example.gaitxplore
 
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView.Orientation
 
 class MainActivity : AppCompatActivity() {
 
-    private var acceleration = FloatArray(3)
-    private var orientation = FloatArray(3)
-    private var angularVelocity = FloatArray(3)
-    private var latitude: Double = 0.0
-    private var longitude: Double = 0.0
-    private var speed: Float = 0f
+    private lateinit var  sensorManager: SensorManager
+    private lateinit var  locationManager: SensorManager
+
+    private lateinit var  xAcceleration : TextView
+    private lateinit var  yAcceleration : TextView
+    private lateinit var  zAcceleration : TextView
+
+
+    private lateinit var  xOrientation : TextView
+    private lateinit var  yOrientation : TextView
+    private lateinit var  zOrientation : TextView
+
+    private lateinit var  xAngularVelocity : TextView
+    private lateinit var  yAngularVelocity : TextView
+    private lateinit var  zAngularVelocity : TextView
+
+
+    private lateinit var  Latitute : TextView
+    private lateinit var  Longitude : TextView
+    private lateinit var  Speed : TextView
+
+
+    private lateinit var  btnStar: Button
+
+    private var IsRecording  =false
 
     
 
+//    private var acceleration = FloatArray(3)
+//    private var orientation = FloatArray(3)
+//    private var angularVelocity = FloatArray(3)
+//    private var latitude: Double = 0.0
+//    private var longitude: Double = 0.0
+//    private var speed: Float = 0f
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main))
+        { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+
+
     }
 }
