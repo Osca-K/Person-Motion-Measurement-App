ff6a1dac8169153fe5ed4c1f6a59f1c5069ae8d4
Osca <oscakholopha@gmail.com>
2024-10-04 22:30:09 +0200

    Fix the indexing issue of Acceleration in Z-direction (Mistake it was 1 instead of 2

--- app/src/main/java/com/example/gaitxplore/MainActivity.kt
+++ app/src/main/java/com/example/gaitxplore/MainActivity.kt
@@ -183,14 +183,14 @@ class MainActivity : AppCompatActivity() , SensorEventListener{
 
                         xAccel=event.values[0].toDouble()
                         yAccel=event.values[1].toDouble()
-                        zAccel=event.values[1].toDouble()
+                        zAccel=event.values[2].toDouble()
 
                         xPitch=Math.toDegrees(atan2(yAccel, sqrt(xAccel*xAccel+zAccel+zAccel)))
                         yRoll=Math.toDegrees(atan2(-xAccel, sqrt(yAccel*yAccel+zAccel*zAccel)))
 
 
                      xOrientation.text = String.format("%.3f", xPitch)
-                     yOrientation.text = String.format("%.3f", yRoll) 
+                     yOrientation.text = String.format("%.3f", yRoll)
 
 
 
