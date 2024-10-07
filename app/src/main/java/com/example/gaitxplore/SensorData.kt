b2494f82139a1bcfade811b181733c21840e8749
Osca <oscakholopha@gmail.com>
2024-10-07 20:23:36 +0200

    Class with parameter inside the constructor

--- app/src/main/java/com/example/gaitxplore/SensorData.kt
+++ app/src/main/java/com/example/gaitxplore/SensorData.kt
@@ -1,4 +1,7 @@
 package com.example.gaitxplore
 
-class SensorData {
-}
+class SensorData(val xAccel: Double, val yAccel: Double, val zAccel: Double, val xRot: Double,
+                 val yRot: Double, val zRot: Double, val xAngularVelocity: Double,
+                 val yAngularVelocity: Double, val zAngularVelocity: Double, val latitude: Double,
+                 val longitude: Double, val speed: Double
+)
